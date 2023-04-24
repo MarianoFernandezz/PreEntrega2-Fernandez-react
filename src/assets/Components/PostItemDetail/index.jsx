b63b28ad69b8e2e77bedf2a77654beb196e8d41a
Mariano Fernandez <mariano_b_i_l_l@hotmail.com>
2023-04-24 16:44:05 -0300

    product detail finish

--- src/assets/Components/PostItemDetail/index.jsx
+++ src/assets/Components/PostItemDetail/index.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './postItemDetail.module.css'
 import { CardMedia } from '@mui/material'
 
-const PostItemDetail = ({products}) => {
+const PostItemDetail = ({ products }) => {
+    const [counter, setCounter] = useState(1);
+    const sumarContador = () => { setCounter(counter + 1) };
+    const restarContador = () => { setCounter(counter > 1 ? counter - 1 : counter) }
     return (
         <div className={style.container}>
             <div className={style.details}>
                 <div className='big-img'>
-                    <img src={products.images} alt="" />
+                    <img src={products.images} height={"400px"} alt="" />
                 </div>
                 <div className={style.box}>
                     <div className={style.row}>
-                        <h2>{ products.title }</h2>
+                        <h2>{products.title}</h2>
                     </div>
-                        <span> $ {products.price} </span>
+                    <span> $ {products.price} </span>
                     <p>DESCRIPCION :</p>
-                    <p>{products.description }</p>
+                    <p>{products.description}</p>
+                    <div className={style.containerCounter}>
+                        <button className={style.button} onClick={restarContador}> - </button>
+                        <span>{counter}</span>
+                        <button className={style.button} onClick={sumarContador}> + </button>
+                    </div>
+                        <button className={style.buttonAddToCard}>ADD TO CART</button>
+
                 </div>
             </div>
         </div>
