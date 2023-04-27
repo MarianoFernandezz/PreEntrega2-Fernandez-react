7c0fcabecb64a41d0b40eb9a29a31fa4adc0eca4
Mariano Fernandez <mariano_b_i_l_l@hotmail.com>
2023-04-27 13:10:43 -0300

    cart table advance

--- src/assets/Components/CardWiew/index.jsx
+++ src/assets/Components/CardWiew/index.jsx
@@ -9,16 +9,40 @@ const CartWiew = () => {
     }
     return (
         <div>
-            {cart.map(product => (
-                <div>
-                    <h2>{product.title}</h2>
-                    <img src={product.images}></img>
-                    <p>${product.price}</p>
-                </div>
-
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map(product => (
+                        <tr>
+                        <td><img className={style.imgTable} src={product.images} alt="Producto"/></td>
+                            <td>{product.title}</td>
+                            <td>${product.price}</td>
+                            <td>{product.quantity}</td>
+                        <td className={style.subtotal}>$20.00</td>
+                        <td className="acciones"><button>Delete</button></td>
+                    </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="4"></td>
+                        <td class="subtotal">$85.00</td>
+                        <td></td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     )
 }
 
 export default CartWiew
+
