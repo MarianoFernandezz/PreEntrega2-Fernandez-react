bd6111c9effa82d1a4cec4135410b6f3671c6213
Mariano Fernandez <mariano_b_i_l_l@hotmail.com>
2023-04-28 22:06:49 -0300

    link to /

--- src/assets/Components/ResponsiveAppBar/index.jsx
+++ src/assets/Components/ResponsiveAppBar/index.jsx
@@ -53,7 +53,8 @@ function ResponsiveAppBar() {
 }} />
                     </Link>
                     <span style={{ color: "inherit" }} >{cart.length}</span>
-                    <Typography
+                    <Link to='/'>
+                            <Typography
                         variant="h6"
                         noWrap
                         component="a"
@@ -65,13 +66,15 @@ function ResponsiveAppBar() {
                             fontFamily: 'monospace',
                             fontWeight: 700,
                             letterSpacing: '.3rem',
-                            color: 'inherit',
+                            color: 'white',
                             textDecoration: 'none',
                         }}
                     >
                         
                         CHERRY STORE 
                     </Typography>
+                    </Link>
+            
                         
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
